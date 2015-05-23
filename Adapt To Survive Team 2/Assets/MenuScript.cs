f1486fd7c086a110908a9199ef1653563c167fa6
gfazenda <gludvigf@uci.edu>
2015-05-23 12:27:43 -0700

    Menu Scene

---
+++ Adapt To Survive Team 2/Assets/MenuScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+
+	public void StartGame(){
+		Application.LoadLevel ("ActualDerek");
+	}
+
+	
+	public void Credits(){
+		Application.LoadLevel ("Credits");
+	}
+
+	
+	public void Quit(){
+		Application.Quit ();
+	}
+
+}
--- Adapt To Survive Team 2/Assets/Scripts/PlayerState.cs
+++ Adapt To Survive Team 2/Assets/Scripts/PlayerState.cs
@@ -8,32 +8,32 @@ public class PlayerState : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKeyDown (KeyCode.I) && animals != 1) 
+		if (Input.GetKeyDown ("1") && animals != 1) 
 		{
 			animals = 1;
 			Debug.Log ("Monkey");
 		}
-		else if (Input.GetKeyDown (KeyCode.J) && animals != 2)
+		else if (Input.GetKeyDown ("2") && animals != 2)
 		{
 			animals = 2;
 			Debug.Log ("Hawk");
 		}
-		else if (Input.GetKeyDown (KeyCode.K) && animals != 3)
+		else if (Input.GetKeyDown ("3") && animals != 3)
 		{
 			animals = 3;
 			Debug.Log ("Dolphin");
 		}
-		else if (Input.GetKeyDown (KeyCode.L) && animals != 4)
+		else if (Input.GetKeyDown ("4") && animals != 4)
 		{
 			animals = 4;
 			Debug.Log ("Bear");
 		}
-		else if (Input.GetKeyDown (KeyCode.U) && animals != 5)
+		else if (Input.GetKeyDown ("5") && animals != 5)
 		{
 			animals = 5;
 			Debug.Log ("Mole");
 		}
-		else if (Input.GetKeyDown (KeyCode.O) && animals != 6)
+		else if (Input.GetKeyDown ("6") && animals != 6)
 		{
 			animals = 6;
 			Debug.Log ("Porcupine");
