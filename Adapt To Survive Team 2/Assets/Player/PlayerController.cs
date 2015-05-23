74b58be4e515caa690b77534e12e9b41dad1a73d
Reddeyfish <edrichd@uci.edu>
2015-05-23 00:37:36 -0700

    added particle effects to the jumps

--- Adapt To Survive Team 2/Assets/Player/PlayerController.cs
+++ Adapt To Survive Team 2/Assets/Player/PlayerController.cs
@@ -9,12 +9,13 @@ public class PlayerController : MonoBehaviour
     private int numLanes = 3;
     private int lane = 1;
     public float laneDistance = 2f;
-
+    private ParticleSystem jumpDust;
     private Controls control;
     // Use this for initialization
     void Start()
     {
         control = GetComponent<Controls>();
+        jumpDust = transform.Find("JumpDustParticleSystem").GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -26,12 +27,17 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Jump!");
             lane--;
             StartCoroutine(UpdateLane());
+            jumpDust.transform.eulerAngles = new Vector3(jumpDust.transform.eulerAngles.x, 90, jumpDust.transform.eulerAngles.z); //turn
+            jumpDust.Play();
+
         }
         else if (control.ConsumeCommandStart(Controls.Command.RIGHT) && lane < numLanes - 1)
         {
             Debug.Log("Jump!");
             lane++;
             StartCoroutine(UpdateLane());
+            jumpDust.transform.eulerAngles = new Vector3(jumpDust.transform.eulerAngles.x, 270, jumpDust.transform.eulerAngles.z); //turn
+            jumpDust.Play();
         }
 
     }
@@ -42,14 +48,20 @@ public class PlayerController : MonoBehaviour
         do
         {
             targetDistance = lane * laneDistance - this.transform.position.x;
-            this.transform.position = this.transform.position + new Vector3(targetDistance / 3, this.transform.position.y, this.transform.position.z);
+            this.transform.position = this.transform.position + new Vector3(targetDistance / 3, 0, this.transform.position.z);
             yield return 0;
         }
         while (Mathf.Abs(targetDistance) > 0.1f);
 
-        this.transform.position = new Vector3(lane * laneDistance, this.transform.position.y, this.transform.position.z);
+        this.transform.position = new Vector3(lane * laneDistance, 0, this.transform.position.z);
 
         
+    }
+
+    //debugging
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(other);
     }
 
 }
