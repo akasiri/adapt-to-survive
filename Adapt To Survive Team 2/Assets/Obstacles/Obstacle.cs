b3868d4a9d50f4a921c2692336a4e6aaabe411f1
Reddeyfish <edrichd@uci.edu>
2015-05-22 23:15:13 -0700

    score system (basics)

---
+++ Adapt To Survive Team 2/Assets/Obstacles/Obstacle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+    void Start()
+    {
+        //temp code
+        GetComponent<Rigidbody2D>().velocity = new Vector3(0, -3, 0);
+    }
+
+	public int weakness = 1;
+	void OnTriggerEnter2D(Collider2D coll) {
+        Debug.Log("Hit!");
+		if (coll.gameObject.tag == "Player") {
+			if (coll.gameObject.GetComponent<PlayerState>().animal == weakness) {
+				// obstacle die
+                OnDestroy();
+			}
+			else {
+				// player die
+			}
+		}
+	}
+
+    protected virtual void OnDestroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Adapt To Survive Team 2/Assets/Score/ScoreScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ScoreScript : MonoBehaviour {
+    private Text text;
+    private float _score = 0;
+    public double Score
+    {
+        get { return _score; }
+    }
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        _score += Time.deltaTime;
+        text.text = _score.ToString();
+	}
+}
