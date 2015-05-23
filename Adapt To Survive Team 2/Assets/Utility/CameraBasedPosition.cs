0e83b5911101b6744c9fe3a46467803d9c5a638c
Reddeyfish <edrichd@uci.edu>
2015-05-23 12:24:53 -0700

    particle system now scales to screen size

---
+++ Adapt To Survive Team 2/Assets/Utility/CameraBasedPosition.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBasedPosition : MonoBehaviour {
+    public float xpos;
+    public float ypos;
+	// Use this for initialization
+	void Start () {
+        this.transform.position = (Vector2)(Camera.main.ViewportToWorldPoint(new Vector3(xpos, ypos, 0)));
+	}
+}
