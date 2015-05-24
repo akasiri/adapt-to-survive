6922083fa26332f785f9274d395fc40a65fb98d3
gfazenda <gludvigf@uci.edu>
2015-05-23 20:21:50 -0700

    Tutorial

---
+++ Adapt To Survive Team 2/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialScript : MonoBehaviour {
+	public GameObject Canvas, Canvas2;
+
+
+
+	public void SecondPage(){
+		Canvas.gameObject.SetActive (false);
+		Canvas2.gameObject.SetActive (true);
+	}
+
+	public void FirstPage(){
+		Canvas.gameObject.SetActive (true);
+		Canvas2.gameObject.SetActive (false);
+	}
+
+
+	public void MainMenu(){
+		Application.LoadLevel ("Menu");
+	}
+}
