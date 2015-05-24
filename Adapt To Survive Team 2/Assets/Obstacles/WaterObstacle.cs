b23d2adbf20fe9dd56e861f9966c2b2a2909bb96
Reddeyfish <edrichd@uci.edu>
2015-05-23 17:05:14 -0700

    minor changes

--- Adapt To Survive Team 2/Assets/Obstacles/WaterObstacle.cs
+++ Adapt To Survive Team 2/Assets/Obstacles/WaterObstacle.cs
@@ -3,19 +3,24 @@ using System.Collections;
 
 public class WaterObstacle : Obstacle {
     private ParticleSystem particle;
+    private bool played = false;
 	// Update is called once per frame
 	protected override void OnDestroy () {
-        GetComponent<AudioSource>().Play();
+        
         GameObject player = GameObject.FindGameObjectWithTag(Tags.Player);
         particle = player.transform.Find("SplashEffect").GetComponent<ParticleSystem>();
         particle.startColor = new Color(1, 1, 1, 0.4f);
         particle.Play();
+        if (!played)
+        {
+            GetComponent<AudioSource>().Play();
+            played = true;
+        }
 	}
 
     void OnTriggerExit2D(Collider2D other)
     {
-        particle.Stop();
-        Destroy(this.gameObject, 0.5f);
+        Destroy(this.gameObject);
     }
 
 
