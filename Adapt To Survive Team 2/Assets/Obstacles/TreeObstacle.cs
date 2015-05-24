e10ec1bfa15942791060b2faec3ade0154028924
Reddeyfish <edrichd@uci.edu>
2015-05-23 17:40:48 -0700

    added audio to trees

---
+++ Adapt To Survive Team 2/Assets/Obstacles/TreeObstacle.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeObstacle : Obstacle
+{
+    protected override void OnDestroy()
+    {
+           GetComponent<AudioSource>().Play();
+    }
+}
